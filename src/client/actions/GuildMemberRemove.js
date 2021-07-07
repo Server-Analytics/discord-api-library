7a607d5e7c4fe39582c49fa1baef46a6d148b1a9
UBrain <ubrainfr@gmail.com>
2021-07-07 05:25:04 +0200

    Cache update

--- src/client/actions/GuildMemberRemove.js
+++ src/client/actions/GuildMemberRemove.js
@@ -1,29 +1,35 @@
 'use strict';
 
 const Action = require('./Action');
-const { Events, Status } = require('../../util/Constants');
+const {
+  Events,
+  Status
+} = require('../../util/Constants');
 
 class GuildMemberRemoveAction extends Action {
   handle(data, shard) {
     const client = this.client;
     const guild = client.guilds.cache.get(data.guild_id);
-    let member = null;
     if (guild) {
-      member = this.getMember({ user: data.user }, guild);
+
       guild.memberCount--;
-      if (member) {
-        member.deleted = true;
-        guild.members.cache.delete(member.id);
-        /**
-         * Emitted whenever a member leaves a guild, or is kicked.
-         * @event Client#guildMemberRemove
-         * @param {GuildMember} member The member that has left/been kicked from the guild
-         */
-        if (shard.status === Status.READY) client.emit(Events.GUILD_MEMBER_REMOVE, member);
-      }
+
+      /**
+       * Emitted whenever a member leaves a guild, or is kicked.
+       * @event Client#guildMemberRemove
+       * @param {GuildMember} member The member that has left/been kicked from the guild
+       */
+      client.emit(Events.GUILD_MEMBER_REMOVE, {
+        userId: data.user_id,
+        guildId: data.guild_id
+      });
+
       guild.voiceStates.cache.delete(data.user.id);
+
     }
-    return { guild, member };
+    return {
+      guild
+    };
   }
 }
 
--- src/client/actions/VoiceStateUpdate.js
+++ src/client/actions/VoiceStateUpdate.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const Action = require('./Action');
-const { Events } = require('../../util/Constants');
+const {
+  Events
+} = require('../../util/Constants');
 const Structures = require('../../util/Structures');
 
 class VoiceStateUpdate extends Action {
@@ -11,25 +13,19 @@ class VoiceStateUpdate extends Action {
     if (guild) {
       const VoiceState = Structures.get('VoiceState');
       // Update the state
-      const oldState = guild.voiceStates.cache.has(data.user_id)
-        ? guild.voiceStates.cache.get(data.user_id)._clone()
-        : new VoiceState(guild, { user_id: data.user_id });
+      const oldState = guild.voiceStates.cache.has(data.user_id) ?
+        guild.voiceStates.cache.get(data.user_id)._clone() :
+        new VoiceState(guild, {
+          user_id: data.user_id
+        });
 
       const newState = guild.voiceStates.add(data);
 
-      // Get the member
-      let member = guild.members.cache.get(data.user_id);
-      if (member && data.member) {
-        member._patch(data.member);
-      } else if (data.member && data.member.user && data.member.joined_at) {
-        member = guild.members.add(data.member);
-      }
+      let stateUpdateType = "";
 
-      // Emit event
-      if (member && member.user.id === client.user.id) {
-        client.emit('debug', `[VOICE] received voice state update: ${JSON.stringify(data)}`);
-        client.voice.onVoiceStateUpdate(data);
-      }
+      if (oldState.channelID && data.channel_id) return;
+      else if (oldState.channelID == null) stateUpdateType = "joined";
+      else stateUpdateType = "left"
 
       /**
        * Emitted whenever a member changes voice state - e.g. joins/leaves a channel, mutes/unmutes.
@@ -37,7 +33,10 @@ class VoiceStateUpdate extends Action {
        * @param {VoiceState} oldState The voice state before the update
        * @param {VoiceState} newState The voice state after the update
        */
-      client.emit(Events.VOICE_STATE_UPDATE, oldState, newState);
+      client.emit(Events.VOICE_STATE_UPDATE, stateUpdateType, {
+        userId: data.user_id,
+        guildId: data.guild_id
+      });
     }
   }
 }
